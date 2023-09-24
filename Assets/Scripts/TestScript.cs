e9197b112aa94d5f2a5349ba47cbb9cde54f3627
yosio_427 <yoshi.yuske@gmail.com>
2023-09-24 22:27:42 +0900

    Testコード追加

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+	int a;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class TestScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        a++;
     }
 }
